f659554f0b19376baae3ba589d5122c7d7875cd3
Poisk <vpisotski@gmail.com>
2018-04-30 13:31:16 -0700

    up to foragerBee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
+
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing'
+  // how to inherit properties from subclass
+  // without assigning new instance to the constructor
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
